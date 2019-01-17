f3f8258023762ddb591921fde71cb8e3f17ba3dd
Porramet Taeng-orn <37680321+porrametict@users.noreply.github.com>
2019-01-17 16:23:51 +0700

    chat Event

--- adonisserve/app/Controllers/Ws/ChatController.js
+++ adonisserve/app/Controllers/Ws/ChatController.js
@@ -11,12 +11,14 @@ class ChatController {
     this.request = request  
   }
 
-
   async onMessage(message){
     //console.log("onMessage")
     // let user = await auth.authercation('user').user ; 
     // console.log("User",user)
-    console.log(message)
+    console.log(message,this.socket.id)
+    arr.push({"user": message.user,"socket": this.socket.id})
+    console.log(arr)
+    
     this.socket.broadcastToAll('message', message)
   }
 }
